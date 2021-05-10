8ef65c8821fe3710df6c0233308ac92056e20d8a
Asebastianriosc <83048937+Asebastianriosc@users.noreply.github.com>
2021-05-10 19:19:18 -0400

    modificacion fuente letra  y color

--- app/src/main/java/com/example/magicball/MainActivity.java
+++ app/src/main/java/com/example/magicball/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String [] respuestaArray ={"Es cierto", "Definitivamente es así", "Sin duda", "Sí definitivamente", "Puede confiar en él", "Como yo lo veo, sí",
             "Lo más probable", "Perspectiva buena", "Sí", "Las señales apuntan a sí", "Respuesta confusa intente de nuevo", "Pregunte de nuevo más tarde",
             "Mejor no decirte ahora", "No puedo predecir ahora", "Concéntrate y pregunta de nuevo", "No cuentes con eso",
-            "Mi respuesta es no", "Mis fuentes dicen que no", "Outlook no es tan bueno", "Muy dudoso"};
+            "Mi respuesta es no", "Mis fuentes dicen que no", "No lo pienses tanto", "Muy dudoso"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
